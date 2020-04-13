750a2cb6cf19a396ad5b7595a9a872d19f1fecb2
Hubks26 <alex.goussian@gmail.com>
2020-04-13 23:27:08 +0200

    Implementation of players

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,13 +3,18 @@
 const sf::Time Game::m_timePerFrame = sf::seconds(1.f / 60.f);
 
 Game::Game()
-: m_window(sf::VideoMode(640, 480), "SFML Application")
+: m_window(sf::VideoMode(1200, 600), "Pong")
 {
-    m_player.setRadius(10.f);
-    m_player.setPosition(m_window.getSize().x/2, m_window.getSize().y/2);
-    sf::FloatRect rect = m_player.getLocalBounds();
-    m_player.setOrigin(rect.left + rect.width/2, rect.top + rect.height/2);
-    m_player.setFillColor(sf::Color::Cyan);
+    m_player1.setSize({10,100});
+    m_player1.setPosition(m_window.getSize().x/15, m_window.getSize().y/2);
+    sf::FloatRect rectPlay1 = m_player1.getLocalBounds();
+    m_player1.setOrigin(rectPlay1.left + rectPlay1.width/2, rectPlay1.top + rectPlay1.height/2);
+    m_player1.setFillColor(sf::Color::Cyan);
+    m_player2.setSize({10,100});
+    m_player2.setPosition(m_window.getSize().x*14/15, m_window.getSize().y/2);
+    sf::FloatRect rectPlay2 = m_player2.getLocalBounds();
+    m_player2.setOrigin(rectPlay2.left + rectPlay2.width/2, rectPlay2.top + rectPlay2.height/2);
+    m_player2.setFillColor(sf::Color::Red);
 }
 
 void Game::run()
@@ -58,37 +63,42 @@ void Game::processEvents()
 
 void Game::update(sf::Time deltaTime)
 {
-    sf::Vector2f movement(0.f, 0.f);
+    sf::Vector2f movementP1(0.f, 0.f);
+    sf::Vector2f movementP2(0.f, 0.f);
+    sf::FloatRect rectPlay1 = m_player1.getGlobalBounds();
+    sf::FloatRect rectPlay2 = m_player2.getGlobalBounds();
     
-    if (m_isMovingUp)
-        movement.y -= m_speed;
-    if (m_isMovingDown)
-        movement.y += m_speed;
-    if (m_isMovingLeft)
-        movement.x -= m_speed;
-    if (m_isMovingRight)
-        movement.x += m_speed;
+    if (m_isP1MovingUp && rectPlay1.top >= 0)
+        movementP1.y -= m_speed;
+    if (m_isP1MovingDown && rectPlay1.top + rectPlay1.height  <= m_window.getSize().y)
+        movementP1.y += m_speed;
+    if (m_isP2MovingUp && rectPlay2.top >= 0)
+        movementP2.y -= m_speed;
+    if (m_isP2MovingDown && rectPlay2.top + rectPlay2.height <= m_window.getSize().y)
+        movementP2.y += m_speed;
     
-    m_player.move(movement * deltaTime.asSeconds());
+    m_player1.move(movementP1 * deltaTime.asSeconds());
+    m_player2.move(movementP2 * deltaTime.asSeconds());
 }
 
 void Game::render()
 {
     m_window.clear();
-    m_window.draw(m_player);
+    m_window.draw(m_player1);
+    m_window.draw(m_player2);
     m_window.display();
 }
 
 void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
 {
     if (key == sf::Keyboard::Z)
-        m_isMovingUp = isPressed;
+        m_isP1MovingUp = isPressed;
     else if (key == sf::Keyboard::S)
-        m_isMovingDown = isPressed;
-    else if (key == sf::Keyboard::Q)
-        m_isMovingLeft = isPressed;
-    else if (key == sf::Keyboard::D)
-        m_isMovingRight = isPressed;
+        m_isP1MovingDown = isPressed;
+    else if (key == sf::Keyboard::O)
+        m_isP2MovingUp = isPressed;
+    else if (key == sf::Keyboard::L)
+        m_isP2MovingDown = isPressed;
 }
 
 
--- src/Game.h
+++ src/Game.h
@@ -21,16 +21,17 @@ class Game
     private:
         
         sf::RenderWindow m_window;
-        sf::CircleShape m_player;
+        sf::RectangleShape m_player1;
+        sf::RectangleShape m_player2;
     
         static const sf::Time m_timePerFrame;
         
-        float m_speed = 170.f;
+        float m_speed = 300.f;
         
-        bool m_isMovingUp;
-        bool m_isMovingDown;
-        bool m_isMovingLeft;
-        bool m_isMovingRight;
+        bool m_isP1MovingUp = false;
+        bool m_isP1MovingDown = false;
+        bool m_isP2MovingUp = false;
+        bool m_isP2MovingDown = false;
 };
 
 #endif // GAME_H
