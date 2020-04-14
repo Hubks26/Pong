1e479489e82feab5fa10c004b92da7bb0f83e962
Hubks26 <alex.goussian@gmail.com>
2020-04-14 14:00:00 +0200

    It works?

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,16 +5,21 @@ const sf::Time Game::m_timePerFrame = sf::seconds(1.f / 60.f);
 Game::Game()
 : m_window(sf::VideoMode(1200, 600), "Pong")
 {
-    m_player1.setSize({10,100});
+    m_player1.setSize({15,130});
     m_player1.setPosition(m_window.getSize().x/15, m_window.getSize().y/2);
-    sf::FloatRect rectPlay1 = m_player1.getLocalBounds();
-    m_player1.setOrigin(rectPlay1.left + rectPlay1.width/2, rectPlay1.top + rectPlay1.height/2);
+    sf::FloatRect rectP1 = m_player1.getLocalBounds();
+    m_player1.setOrigin(rectP1.left + rectP1.width/2, rectP1.top + rectP1.height/2);
     m_player1.setFillColor(sf::Color::Cyan);
-    m_player2.setSize({10,100});
+    m_player2.setSize({15,130});
     m_player2.setPosition(m_window.getSize().x*14/15, m_window.getSize().y/2);
-    sf::FloatRect rectPlay2 = m_player2.getLocalBounds();
-    m_player2.setOrigin(rectPlay2.left + rectPlay2.width/2, rectPlay2.top + rectPlay2.height/2);
+    sf::FloatRect rectP2 = m_player2.getLocalBounds();
+    m_player2.setOrigin(rectP2.left + rectP2.width/2, rectP2.top + rectP2.height/2);
     m_player2.setFillColor(sf::Color::Red);
+    m_ball.setRadius(10.f);
+    m_ball.setPosition(m_window.getSize().x/2, 0.f);
+    sf::FloatRect rectBall = m_ball.getLocalBounds();
+    m_ball.setOrigin(rectBall.left + rectBall.width/2, rectBall.top);
+    m_ball.setFillColor(sf::Color::White);
 }
 
 void Game::run()
@@ -29,7 +34,6 @@ void Game::run()
         while (timeSinceLastUpdate > m_timePerFrame)
         {
             timeSinceLastUpdate -= m_timePerFrame;
-            processEvents();
             update(m_timePerFrame);
         }
         render();
@@ -41,44 +45,44 @@ void Game::processEvents()
     sf::Event event;
     while (m_window.pollEvent(event))
     {
-        switch (event.type)
-        {
-            case sf::Event::KeyPressed:
-                handlePlayerInput(event.key.code, true);
-                break;
-                
-            case sf::Event::KeyReleased:
-                handlePlayerInput(event.key.code, false);
-                break;
-                
-            case sf::Event::Closed:
-                m_window.close();
-                break;
-                
-            default:
-                break;
-        }
+        
+        if (event.type == sf::Event::Closed)
+            m_window.close();
     }
 }
 
 void Game::update(sf::Time deltaTime)
 {
+    handlePlayerInput();
     sf::Vector2f movementP1(0.f, 0.f);
     sf::Vector2f movementP2(0.f, 0.f);
-    sf::FloatRect rectPlay1 = m_player1.getGlobalBounds();
-    sf::FloatRect rectPlay2 = m_player2.getGlobalBounds();
+    sf::Vector2f movementBall(0.f, 0.f);
+    if (!m_ballStartMovingForP2 && (m_isP1MovingUp || m_isP1MovingDown))
+        m_ballStartMovingForP1 = true;
+    if (m_ballStartMovingForP1)
+        movementBall = {-m_speedBall, m_speedBall};
+    if (!m_ballStartMovingForP1 && (m_isP2MovingUp || m_isP2MovingDown))
+        m_ballStartMovingForP2 = true;
+    if (m_ballStartMovingForP2)
+        movementBall = {m_speedBall, m_speedBall};
     
-    if (m_isP1MovingUp && rectPlay1.top >= 0)
-        movementP1.y -= m_speed;
-    if (m_isP1MovingDown && rectPlay1.top + rectPlay1.height  <= m_window.getSize().y)
-        movementP1.y += m_speed;
-    if (m_isP2MovingUp && rectPlay2.top >= 0)
-        movementP2.y -= m_speed;
-    if (m_isP2MovingDown && rectPlay2.top + rectPlay2.height <= m_window.getSize().y)
-        movementP2.y += m_speed;
+    sf::FloatRect rectP1 = m_player1.getGlobalBounds();
+    sf::FloatRect rectP2 = m_player2.getGlobalBounds();
+    
+    float tickMovement = m_speedPlayers * m_timePerFrame.asSeconds();
+
+    if (m_isP1MovingUp && rectP1.top >= tickMovement)
+        movementP1.y -= m_speedPlayers;
+    if (m_isP1MovingDown && rectP1.top + rectP1.height  <= m_window.getSize().y - tickMovement)
+        movementP1.y += m_speedPlayers;
+    if (m_isP2MovingUp && rectP2.top >= tickMovement)
+        movementP2.y -= m_speedPlayers;
+    if (m_isP2MovingDown && rectP2.top + rectP2.height <= m_window.getSize().y - tickMovement)
+        movementP2.y += m_speedPlayers;
     
     m_player1.move(movementP1 * deltaTime.asSeconds());
     m_player2.move(movementP2 * deltaTime.asSeconds());
+    m_ball.move(movementBall * deltaTime.asSeconds());
 }
 
 void Game::render()
@@ -86,19 +90,16 @@ void Game::render()
     m_window.clear();
     m_window.draw(m_player1);
     m_window.draw(m_player2);
+    m_window.draw(m_ball);
     m_window.display();
 }
 
-void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+void Game::handlePlayerInput()
 {
-    if (key == sf::Keyboard::Z)
-        m_isP1MovingUp = isPressed;
-    else if (key == sf::Keyboard::S)
-        m_isP1MovingDown = isPressed;
-    else if (key == sf::Keyboard::O)
-        m_isP2MovingUp = isPressed;
-    else if (key == sf::Keyboard::L)
-        m_isP2MovingDown = isPressed;
+    m_isP1MovingUp = sf::Keyboard::isKeyPressed(sf::Keyboard::Z);
+    m_isP1MovingDown = sf::Keyboard::isKeyPressed(sf::Keyboard::X);
+    m_isP2MovingUp = sf::Keyboard::isKeyPressed(sf::Keyboard::U);
+    m_isP2MovingDown = sf::Keyboard::isKeyPressed(sf::Keyboard::N);
 }
 
 
--- src/Game.h
+++ src/Game.h
@@ -16,22 +16,27 @@ class Game
         void update(sf::Time);
         void render();
         
-        void handlePlayerInput(sf::Keyboard::Key, bool);
+        void handlePlayerInput();
         
     private:
         
         sf::RenderWindow m_window;
         sf::RectangleShape m_player1;
         sf::RectangleShape m_player2;
+        sf::CircleShape m_ball;
     
         static const sf::Time m_timePerFrame;
         
-        float m_speed = 300.f;
+        float m_speedPlayers = 450.f;
+        float m_speedBall = 400.f;
         
         bool m_isP1MovingUp = false;
         bool m_isP1MovingDown = false;
         bool m_isP2MovingUp = false;
         bool m_isP2MovingDown = false;
+        
+        bool m_ballStartMovingForP1 = false;
+        bool m_ballStartMovingForP2 = false;
 };
 
 #endif // GAME_H
